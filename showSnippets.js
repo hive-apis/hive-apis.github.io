cc7accb3f9448d8240a4d76a1c807cb12b9530fe
Noah Miles <noah.miles@hivemedia.com>
2023-05-25 13:41:34 -0700

    split snippets into own file store cold copy

---
+++ showSnippets.js
@@ -0,0 +1,52 @@
+const oldManifest = getManifest();
+document.body.appendChild(createElement(oldManifest));
+const manifest = await fetch("snippets-manifest.json"),
+    manifestObj = await manifest.json();
+if (JSON.stringify(oldManifest) !== JSON.stringify(manifestObj)) {
+    setManifest(manifestObj);
+    updateElement(manifestObj);
+}
+
+function getManifest() {
+    try {
+        return JSON.parse(localStorage.getItem("manifest") || {});
+    } catch (e) {
+        return {};
+    }
+}
+
+function setManifest(manifestObj) {
+    localStorage.setItem("manifest", JSON.stringify(manifestObj));
+}
+
+function updateElement(manifestObj) {
+    const newObj = createElement(manifestObj);
+    document.body.removeChild(document.getElementById("manifest"));
+    document.body.appendChild(newObj);
+}
+
+function createElement(manifestObj) {
+    const manifestNode = document.createElement("ul");
+    manifestNode.setAttribute("id", "manifest");
+
+    Object.keys(manifestObj).forEach((name) => {
+        const li = document.createElement("li"),
+            a = document.createElement("a"),
+            span = document.createElement("span");
+        a.innerHTML = `${name}`;
+        span.innerHTML = `${manifestObj[name]}`;
+        li.appendChild(a);
+        li.appendChild(span);
+        li.addEventListener("click", () => {
+            span.classList.toggle("show");
+        });
+        manifestNode.appendChild(li);
+    });
+    return manifestNode;
+}
+
+document.getElementById("switchPage").addEventListener("click", () => {
+    setTimeout(() => {
+        window.location.href = "/report-a-bug.html";
+    }, 500);
+});
